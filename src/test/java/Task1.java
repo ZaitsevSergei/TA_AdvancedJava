cba6b5ec122ea91e3070b68b8d57f3172a17da7d
Sergei Zaitsev <Sergei_Zaitsev@epam.com>
2018-01-15 15:51:20 +0300

    refactoring

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -23,6 +23,20 @@ public class Task1 {
         Assert.assertEquals(WebDriverTools.driver.getTitle(), "Index Page");
 
         // 4. Perform login
+        performLogin();
+
+        // 5. Assert User name in the left-top side of screen that user is loggined
+        assertUserName();
+
+        // 6. Assert Browser title
+        Assert.assertEquals(WebDriverTools.driver.getTitle(), "Index Page");
+
+        WebDriverTools.driver.close();
+    }
+
+    // 4. Perform login
+    private void performLogin()
+    {
         // click on link to display login form
         //SeleniumSetMethods.click(How.XPATH, "//a[@href='#' AND @class='dropdown-toggle']");
         SeleniumSetMethods.click(How.XPATH, "//a[@href='#']");
@@ -31,21 +45,17 @@ public class Task1 {
         SeleniumSetMethods.enterText(How.ID, "Password", "1234");
         // Submit data
         SeleniumSetMethods.click(How.XPATH, "//button[@class='uui-button dark-blue btn-login']");
+    }
 
-        // 5. Assert User name in the left-top side of screen that user is loggined
+    // 5. Assert User name in the left-top side of screen that user is logged
+    private void assertUserName() {
         // Assert User name is visible
         boolean userNamevisibility = WebDriverTools.findElement(How.XPATH, "//div[@class='profile-photo']//span").isDisplayed();
         Assert.assertEquals( userNamevisibility, true);
         // get User name
         String userName = SeleniumGetMethods.getTagInnerHTML(How.XPATH, "//div[@class='profile-photo']//span");
         Assert.assertEquals(userName, "Piter Chailovskii");
-
-        // 6. Assert Browser title
-        Assert.assertEquals(WebDriverTools.driver.getTitle(), "Index Page");
-
-        WebDriverTools.driver.close();
     }
-
     // 2.Open test site by URL
     @BeforeTest
     public void setUpWebDriver()
