1c7948b61b7ce5cc9a56a1f6d7501244595f9b90
Sergei Zaitsev <Sergei_Zaitsev@epam.com>
2018-01-16 12:51:42 +0300

    Created packages for task2 and task2.Exercise1. Passed index page test (Ex.1) with dataProvider but in non parallel mode

---
+++ src/test/java/Task2/Exercise1/Exercise1.java
@@ -0,0 +1,53 @@
+package Task2.Exercise1;
+
+import Framework.SeleniumGetMethods;
+import Framework.WebDriverTools;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.How;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.setProperty;
+import static org.testng.Assert.*;
+
+public class Exercise1 {
+
+    List<String> texts;
+    @DataProvider
+    private Object[][] textsDP()
+    {
+        return new Object[][]
+                {
+                        {0, "To include good practices\nand ideas from successful\nEPAM projec"},
+                        {1, "To be flexible and\ncustomizable"},
+                        {2, "To be multiplatform"},
+                        {3, "Already have good base\n(about 20 internal and\nsome external projects),\n" +
+                                "wish to get more…"}
+                };
+    }
+
+    @BeforeTest
+    public void setUp()
+    {
+        setProperty("webdriver.chrome.driver", "C:/Selenium/chromedriver.exe");
+        WebDriverTools.driver = new ChromeDriver();
+        WebDriverTools.driver.manage().window().maximize();
+        WebDriverTools.driver.navigate().to("https://jdi-framework.github.io/tests/index.htm");
+
+        texts = SeleniumGetMethods.getListOfElementsInnerText(How.XPATH, "//span[@class='benefit-txt']");
+    }
+
+    @Test(dataProvider = "textsDP")
+    public void TestIndexPageTexts(int index, String assertString)
+    {
+        assertEquals(texts.get(index), assertString);
+    }
+
+
+}
