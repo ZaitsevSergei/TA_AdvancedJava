939b5326baa43a84b6db74349b14f08a08a76312
Sergei Zaitsev <Sergei_Zaitsev@epam.com>
2018-01-18 17:01:10 +0300

    WebDriverTools removed from task2

---
+++ src/main/java/base/TestBase.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import static java.lang.System.setProperty;
+
+public abstract class TestBase {
+
+    public static WebDriver driver;
+    public static String driverPath = "windows-drivers/chromedriver.exe";
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() {
+        setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        driver.close();
+    }
+}
