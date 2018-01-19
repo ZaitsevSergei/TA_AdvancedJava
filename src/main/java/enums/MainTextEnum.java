47947a2b758bb5918705f3a628004cbe089b8046
Sergei Zaitsev <Sergei_Zaitsev@epam.com>
2018-01-19 15:12:15 +0300

    Exercise 2.Implemented enum with benefits texts

--- src/main/java/enums/MainTextEnum.java
+++ src/main/java/enums/MainTextEnum.java
@@ -1,4 +1,20 @@
 package enums;
 
-public class MainTextEnum {
+public enum  MainTextEnum {
+    TEXT1("To include good practices\nand ideas from successful\nEPAM projec"),
+    TEXT2("To be flexible and\ncustomizable"),
+    TEXT3("To be multiplatform"),
+    TEXT4("Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+    private final String text;
+
+    MainTextEnum(String text) {
+        this.text = text;
+
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
